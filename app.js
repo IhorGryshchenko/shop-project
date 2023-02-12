8537ef32e81252a1a7a5319e68cdda98ff9b590f
IhorGryshchenko <shaman5324@gmail.com>
2023-02-12 23:04:09 +0100

    add modal window and add libruary jqwery

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
+//add to cart
+
 let productsCountEl = document.querySelector("#products-count");
-console.log(productsCountEl)
-
 let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
-console.log( addToCartBtns);
-
-/* addToCartBtns.addEventListener("click", function() {
-    console.log("clicked")
-})
- */
-
-
-
-/* for(let i= 0;i < addToCartBtns.length; i++ ){
-    addToCartBtns[i].addEventListener("click", function() {
-        console.log("clicked")
-    })
-}; */
-
-
 
 addToCartBtns.forEach((btn)=>btn.addEventListener("click",function () {
     productsCountEl.textContent =  +productsCountEl.textContent +1 ;
    
 }));
+
+//modal
+
+let moreDetailsBtns = document.querySelectorAll(".btn-more-detalis");
+let modal = document.querySelector(".modal");
+let btnClose = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+
+btnClose.addEventListener("click", closeModal);
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+}
+
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+}
+
+modal.addEventListener("click",function (e) {
+    if (e.target === modal) {
+        closeModal ();
+    }
+})
+ 
+
+
+// change like state
+
+let likeBtns = document.querySelectorAll(".like-btn");
+//console.log(likeBtns);
+
+//first variant
+
+likeBtns.forEach((item) => item.addEventListener("click", function () {
+    /* if (item.classList.contains("liked")) {
+        item.classList.remove("liked");
+    } else {
+        item.classList.add("liked")
+    } */
+
+    //second variant
+
+    item.classList.toggle("liked");
+}));
+
+// slick
+
+$(".slider-block").slick();
