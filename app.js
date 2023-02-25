c36096f8b6b1fe423b2a2939af707fd9463c0e2a
IhorGryshchenko <shaman5324@gmail.com>
2023-02-25 22:14:14 +0100

    add new files

--- app.js
+++ app.js
@@ -75,3 +75,38 @@ $(".slider-block").slick({
 
 //AOS.init();
 
+const decrementBtns = document.querySelectorAll(".decrement-button")[0];
+const incrementBtns = document.querySelectorAll(".increment-button")[0];
+const productsInput = document.querySelectorAll(".product-quantity input")[0];
+
+function Counter(incrementBtns,decrementBtns,inputField) {
+    this.domRefs = {
+        incrementBtns,
+        decrementBtns,
+        inputField,
+    };
+
+    this.toggleButtonState = function () {
+        let count = this.domRefs.inputField.value;
+        this.domRefs.decrementBtns.disabled = count <= 1;
+        this.domRefs.incrementBtns.disabled = count >= 10;
+    };
+
+    this.toggleButtonState();
+
+    this.increment = function () {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value +1;
+        this.toggleButtonState();
+    };
+
+    this.decrement = function () {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value -1;
+        this.toggleButtonState();
+    };
+
+    this.domRefs.incrementBtns.addEventListener("click", this.increment.bind(this));
+    this.domRefs.decrementBtns.addEventListener("click", this.decrement.bind(this));
+}
+
+const counter1 = new Counter(incrementBtns,decrementBtns,productsInput);
+//console.log(counter1);
